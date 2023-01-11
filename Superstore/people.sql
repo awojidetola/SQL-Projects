ccc5fda035e89adb83391ee7d816b6e03153a34f
Awojide Margaret <170806003@live.unilag.edu.ng>
2023-01-11 17:10:27 +0100

    Superstore Analysis

---
+++ Superstore/people.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE people 
+(
+    Person	VARCHAR(512),
+    Region	VARCHAR(512)
+);
+
+INSERT INTO people (Person, Region) VALUES ('Anna Andreadi', 'West');
+INSERT INTO people (Person, Region) VALUES ('Chuck Magee', 'East');
+INSERT INTO people (Person, Region) VALUES ('Kelly Williams', 'Central');
+INSERT INTO people (Person, Region) VALUES ('Cassandra Brandow', 'South');
